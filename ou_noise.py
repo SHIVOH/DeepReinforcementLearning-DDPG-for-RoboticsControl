fac1bb2e9ee14783a69a3a7c2066b2b1bb23c55a
Shivoh <46846644+SHIVOH@users.noreply.github.com>
2021-02-08 13:12:33 +0000

    Add files via upload

---
+++ ou_noise.py
@@ -0,0 +1,26 @@
+import numpy as np
+import random
+import copy 
+
+class OUNoise():
+    """ Ornstein-Uhlenbeck process """
+
+    def __init__(self, size, seed, mu=0.0, theta=0.15, sigma=0.2):
+        """ Initialize parameters and noise process """
+        self.mu = mu * np.ones(size)
+        self.theta = theta 
+        self.sigma = sigma
+        self.seed = random.seed(seed)
+        self.size = size
+        self.reset()
+
+    def reset(self):
+        """ Reset the interal state (= noise) to mean (mu). """
+        self.state = copy.copy(self.mu)
+
+    def sample(self):
+        """ Update internal state and return it as a noise sample """
+        x = self.state 
+        dx = self.theta * (self.mu - x) + self.sigma * np.random.standard_normal(self.size)
+        self.state = x + dx 
+        return self.state
